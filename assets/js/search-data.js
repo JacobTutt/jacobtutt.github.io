37527b41ef13413fbc7b53d83e95b1c91aeac766
JacobTutt <jacobtutt@icloud.com>
2025-08-26 14:35:35 +0000

    Deploying to gh-pages from @ JacobTutt/jacobtutt.github.io@3d2b490d7afd8a55613c5aad869e21eb6958318d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Please find an outline (and PDF) of my academic CV, focusing on my academic background.   For a more general or industry-focused CV, feel free to contact me for the latest version.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
